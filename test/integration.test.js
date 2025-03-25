44194060ce68dc25c6e26114e561f40947f2bd4b
shreyasingh1354 <133848560+shreyasingh1354@users.noreply.github.com>
2025-03-25 16:11:40 +0530

    Update integration.test.js

--- test/integration.test.js
+++ test/integration.test.js
@@ -1,20 +1,17 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const expect = chai.expect;
 
 chai.use(chaiHttp);
+const { expect } = chai;
 
-describe('Integration Test: Nginx Web Server', () => {
-  it('should return status 200 and the greeting from the HTML file', (done) => {
-    chai.request('http://localhost:8080')  // Corrected URL string
-      .get('/')
-      .end((err, res) => {
-        if (err) {
-          return done(err); // Ensure errors are properly handled
-        }
-        expect(res).to.have.status(200);  // Ensure the server returns 200 status code
-        expect(res.text).to.include('Hello from Docker!');  // Ensure the HTML content is correct
-        done();
-      });
-  });
+describe('Integration Test', function () {
+    it('should return a 200 status for the homepage', function (done) {
+        chai.request('http://localhost:8080') // Make sure this URL is properly formatted
+            .get('/')
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res).to.have.status(200);
+                done();
+            });
+    });
 });
